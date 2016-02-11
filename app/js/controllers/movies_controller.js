53ad547c1767664eff96db9887aa06c84c58f3bd
Gabriel Redig <glredig@Gabriels-MBP-3.home>
2016-02-11 01:59:55 -0800

    populate cast and crew info

--- app/js/controllers/movies_controller.js
+++ app/js/controllers/movies_controller.js
@@ -15,10 +15,36 @@ app.controller('MoviesController', function($scope, $http) {
 				$scope.selected_movie = $scope.movies[0];
 
 				$scope.movies.forEach(function(movie) {
+					movie.writers = [];
+					movie.stars = [];
+					movie.actors = [];
+
 					$http.get('https://api.themoviedb.org/3/movie/' + movie.id + '?api_key=' + api_key)
 						.then(function(data) {
 							movie.poster_url = base_url + 'w185' + data.data.poster_path;
 							movie.thumb_url = base_url + 'w154' + data.data.poster_path;
+						});
+
+					$http.get('https://api.themoviedb.org/3/movie/' + movie.id + '/credits?api_key=' + api_key)
+						.then(function(data) {
+							data.data.crew.forEach(function(member) {
+								if (member.job === 'Director') {
+									movie.director = member.name;
+
+								}
+								else if (member.job === 'Writer') {
+									movie.writers.push(member.name);
+								}
+							});
+
+							data.data.cast.forEach(function(star) {
+								var actor = {};
+
+								actor.name = star.name;
+
+								movie.actors.push(actor);
+								movie.stars.push(actor.name);
+							});
 						});
 				});
 			});
