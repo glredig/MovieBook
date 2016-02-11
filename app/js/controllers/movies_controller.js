0377badcaa976fdb76525a9ccb8ec9d465c98a9c
Gabriel Redig <glredig@Gabriels-MBP-3.home>
2016-02-11 01:17:20 -0800

    add more movie info and styling

--- app/js/controllers/movies_controller.js
+++ app/js/controllers/movies_controller.js
@@ -1,14 +1,30 @@
 app.controller('MoviesController', function($scope, $http) {
+	var base_url;
 	$scope.movies = [];
 
-	$http.get('https://api.themoviedb.org/3/collection/528?api_key=' + api_key)
+	$http.get('https://api.themoviedb.org/3/configuration?api_key=' + api_key)
 		.then(function(data) {
-			$scope.movies = data.data.parts;
-			$scope.selected_movie = $scope.movies[0];
+			base_url = data.data.images.base_url;
+			populateMovies();
 		});
+
+	function populateMovies() {
+		$http.get('https://api.themoviedb.org/3/collection/528?api_key=' + api_key)
+			.then(function(data) {
+				$scope.movies = data.data.parts;
+				$scope.selected_movie = $scope.movies[0];
+
+				$scope.movies.forEach(function(movie) {
+					$http.get('https://api.themoviedb.org/3/movie/' + movie.id + '?api_key=' + api_key)
+						.then(function(data) {
+							movie.poster_url = base_url + 'w185' + data.data.poster_path;
+							movie.thumb_url = base_url + 'w154' + data.data.poster_path;
+						});
+				});
+			});
+	}
 
 	$scope.updateSelected = function(movie) {
 		$scope.selected_movie = movie;
-		console.log(movie);
 	};
 });
