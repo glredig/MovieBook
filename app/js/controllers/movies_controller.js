be15fabf36ff908248a50b848311fbafca0ebf81
Gabriel Redig <glredig@Gabriels-MBP-3.home>
2016-02-13 03:00:37 -0800

    add selection functionality for actors

--- app/js/controllers/movies_controller.js
+++ app/js/controllers/movies_controller.js
@@ -1,9 +1,10 @@
-app.controller('MoviesController', function($scope, $http) {
+app.controller('MoviesController', function($scope, $http, $q) {
 	var base_url;
 	$scope.movies = [];
 
 	$http.get('https://api.themoviedb.org/3/configuration?api_key=' + api_key)
 		.then(function(data) {
+			console.log(data);
 			base_url = data.data.images.base_url;
 			populateMovies();
 		});
@@ -11,6 +12,8 @@ app.controller('MoviesController', function($scope, $http) {
 	function populateMovies() {
 		$http.get('https://api.themoviedb.org/3/collection/528?api_key=' + api_key)
 			.then(function(data) {
+				var credits_promises = [];
+				
 				$scope.movies = data.data.parts;
 				$scope.selected_movie = $scope.movies[0];
 
@@ -25,7 +28,7 @@ app.controller('MoviesController', function($scope, $http) {
 							movie.thumb_url = base_url + 'w154' + data.data.poster_path;
 						});
 
-					$http.get('https://api.themoviedb.org/3/movie/' + movie.id + '/credits?api_key=' + api_key)
+					credits_promises.push($http.get('https://api.themoviedb.org/3/movie/' + movie.id + '/credits?api_key=' + api_key)
 						.then(function(data) {
 							data.data.crew.forEach(function(member) {
 								if (member.job === 'Director') {
@@ -41,16 +44,40 @@ app.controller('MoviesController', function($scope, $http) {
 								var actor = {};
 
 								actor.name = star.name;
+								actor.character = star.character;
+
+								if (star.profile_path) {
+									actor.thumb_url = base_url + '/w185' + star.profile_path;
+									actor.image_url = base_url + '/h632' + star.profile_path;
+								}
+								else {
+									actor.thumb_url = actor.image_url = '';
+								}
 
 								movie.actors.push(actor);
-								movie.stars.push(actor.name);
+								if (movie.stars.length < 5) {
+									movie.stars.push(actor.name);									
+								}
 							});
-						});
+						}));
+				});
+
+				// When all credits are fetched, set the initial selected actor
+				$q.all(credits_promises).then(function() {
+					$scope.selected_actor = $scope.selected_movie.actors[0]
+					;
 				});
 			});
 	}
 
-	$scope.updateSelected = function(movie) {
+	$scope.updateSelectedMovie = function(movie) {
 		$scope.selected_movie = movie;
+
+		// Make sure to select first actor when switching movies
+		$scope.updateSelectedActor(movie.actors[0]);
 	};
+
+	$scope.updateSelectedActor = function(actor) {
+		$scope.selected_actor = actor;
+	}
 });
