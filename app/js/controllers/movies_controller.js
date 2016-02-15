7b3e9e3d3adc5d2ef4bfd62d1b538bdb41414256
Gabriel Redig <glredig@Gabriels-MBP-3.home>
2016-02-14 16:39:50 -0800

    move more gets to services

--- app/js/controllers/movies_controller.js
+++ app/js/controllers/movies_controller.js
@@ -7,72 +7,70 @@ app.controller('MoviesController', function($scope, $http, $q, movieService) {
 			base_url = url;
 			populateMovies();
 		})
-	// $http.get('https://api.themoviedb.org/3/configuration?api_key=' + api_key)
-	// 	.then(function(data) {
-	// 		console.log(data);
-	// 		base_url = data.data.images.base_url;
-	// 		populateMovies();
-	// 	});
 
 	function populateMovies() {
-		$http.get('https://api.themoviedb.org/3/collection/528?api_key=' + api_key)
-			.then(function(data) {
-				var credits_promises = [];
+		movieService.getCollection()
+			.then(function(movies) {
+				$scope.movies = movies;
+				$scope.selected_movie = movies[0];
+
+				populateCredits();
+			})
+	}
+
+	function populateCredits() {
+		var credits_promises = [];
 				
-				$scope.movies = data.data.parts;
-				$scope.selected_movie = $scope.movies[0];
+		$scope.movies.forEach(function(movie) {
+			movie.writers = [];
+			movie.stars = [];
+			movie.actors = [];
 
-				$scope.movies.forEach(function(movie) {
-					movie.writers = [];
-					movie.stars = [];
-					movie.actors = [];
-
-					$http.get('https://api.themoviedb.org/3/movie/' + movie.id + '?api_key=' + api_key)
-						.then(function(data) {
-							movie.poster_url = base_url + 'w185' + data.data.poster_path;
-							movie.thumb_url = base_url + 'w154' + data.data.poster_path;
-						});
-
-					credits_promises.push($http.get('https://api.themoviedb.org/3/movie/' + movie.id + '/credits?api_key=' + api_key)
-						.then(function(data) {
-							data.data.crew.forEach(function(member) {
-								if (member.job === 'Director') {
-									movie.director = member.name;
-
-								}
-								else if (member.job === 'Writer') {
-									movie.writers.push(member.name);
-								}
-							});
-
-							data.data.cast.forEach(function(star) {
-								var actor = {};
-
-								actor.name = star.name;
-								actor.character = star.character;
-
-								if (star.profile_path) {
-									actor.thumb_url = base_url + '/w185' + star.profile_path;
-									actor.image_url = base_url + '/h632' + star.profile_path;
-								}
-								else {
-									actor.thumb_url = actor.image_url = '';
-								}
-
-								movie.actors.push(actor);
-								if (movie.stars.length < 5) {
-									movie.stars.push(actor.name);									
-								}
-							});
-						}));
+			movieService.getMovie(movie.id, base_url)
+				.then(function(images) {
+					movie.poster_url = images.poster_url;
+					movie.thumb_url = images.thumb_url;
 				});
 
-				// When all credits are fetched, set the initial selected actor
-				$q.all(credits_promises).then(function() {
-					$scope.selected_actor = $scope.selected_movie.actors[0]
-					;
-				});
-			});
+			credits_promises.push($http.get('https://api.themoviedb.org/3/movie/' + movie.id + '/credits?api_key=' + api_key)
+				.then(function(data) {
+					data.data.crew.forEach(function(member) {
+						if (member.job === 'Director') {
+							movie.director = member.name;
+
+						}
+						else if (member.job === 'Writer') {
+							movie.writers.push(member.name);
+						}
+					});
+
+					data.data.cast.forEach(function(star) {
+						var actor = {};
+
+						actor.name = star.name;
+						actor.character = star.character;
+
+						if (star.profile_path) {
+							actor.thumb_url = base_url + '/w185' + star.profile_path;
+							actor.image_url = base_url + '/h632' + star.profile_path;
+						}
+						else {
+							actor.thumb_url = actor.image_url = '';
+						}
+
+						movie.actors.push(actor);
+						if (movie.stars.length < 5) {
+							movie.stars.push(actor.name);									
+						}
+					});
+				}));
+		});
+
+		// When all credits are fetched, set the initial selected actor
+		$q.all(credits_promises).then(function() {
+			$scope.selected_actor = $scope.selected_movie.actors[0]
+			;
+		});
 	}
 
 	$scope.updateSelectedMovie = function(movie) {
--- app/js/services/movieService.js
+++ app/js/services/movieService.js
@@ -5,4 +5,21 @@ app.service('movieService', function($http, $q) {
 				return data.data.images.base_url;
 			});
 	};
+
+	this.getCollection = function() {
+		return $http.get('https://api.themoviedb.org/3/collection/528?api_key=' + api_key)
+			.then(function(data) {
+				return data.data.parts;
+			});
+	};
+
+	this.getMovie = function(id, base_url) {
+		return $http.get('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + api_key)
+			.then(function(data) {
+				return {
+					poster_url: base_url + 'w185' + data.data.poster_path,
+					thumb_url: base_url + 'w154' + data.data.poster_path
+				}
+			});
+	}
 })
